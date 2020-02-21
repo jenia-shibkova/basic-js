2e388ce40f08c76c3d7e259369368519a8b952fa
Eugenia Shibkova <shibkova91@gmail.com>
2020-02-21 03:16:10 +0300

    feat: add transform-array partial solution (1 test failing)

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,29 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error;
+  }
+
+  const result = [];
+
+  arr.forEach((el, index) => {
+    switch (el) {
+      case '--discard-next':
+        result.splice(index + 1, 1);
+        break;
+      case '--discard-prev':
+        result.pop();
+        break;
+      case '--double-next':
+        result.push(arr[index + 1]);
+        break;
+      case '--double-prev':
+        result.push(arr[index - 1]);
+        break;     
+      default:
+        result.push(el);
+        break;
+    }       
+  });
+  
+  return result;
 };
