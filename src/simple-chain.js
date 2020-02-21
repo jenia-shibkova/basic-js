e46ae3f8f98d6046707c6c3a7a41514a5c58462e
Eugenia Shibkova <shibkova91@gmail.com>
2020-02-21 17:03:20 +0300

    feat: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,30 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const n = value !== null ? `${value}` : 'null';
+    this.chain.push(`( ${n} )`);
+    return this;    
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position <= 0 || typeof position !== 'number' || position % 1 !== 0 || position >= this.chain.length) {
+      this.chain = [];
+      throw new Error;
+    }
+
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const result = this.chain.join('~~');
+    this.chain = [];
+    return result;
   }
 };
 
