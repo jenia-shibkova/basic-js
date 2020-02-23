736d3b29c321ef5cb09ea1919de483a8cf81ae02
Eugenia Shibkova <shibkova91@gmail.com>
2020-02-23 20:23:03 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,57 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  constructor(value = true) {
+    this.alphabet = new Array(26).fill(1).map(( _, index) => String.fromCharCode(65 + index));
+    this.amount = 26;
+    this.direction = value;
+  }
+  encrypt(str, key) {
+    if (!str|| !key) throw new Error();
+    return this.vigenerCipherFunc(str, key, 'encrypt');   
+  }  
+
+  decrypt(str, key) {
+    if (!str || !key) throw new Error();
+    return this.vigenerCipherFunc(str, key, 'decrypt');
+  }
+
+  getFullKey(str, key) {
+    return (str.length <= key.length) ? key : this.getFullKey(str, key + key);
+  }
+
+  vigenerCipherFunc(str, key, mode) {
+    let fullKey = this.getFullKey(str, key).toUpperCase().split('');
+    const keyLength = str.match(/[A-Z]/gi).length;
+    const message = str.toUpperCase().split('');
+    fullKey.length = keyLength;
+    
+    let result = [];
+    let length = message.length;    
+    let keyI = 0; // key index shift
+
+    for (let i = 0; i <= length; i += 1) {    
+      if (!this.alphabet.includes(message[i])) { // excludes extra characters
+        result.push(message[i]);
+      } else { 
+             
+        const messIndex = this.alphabet.indexOf(message[i]);
+        const keyIndex = this.alphabet.indexOf(fullKey[keyI]);
+      
+        if (mode === 'encrypt') {
+          const cryptValue = this.alphabet[(messIndex + keyIndex) % this.amount];
+          result.push(cryptValue);
+        }
+
+        if (mode === 'decrypt') {
+          const cryptValue = this.alphabet[(messIndex + this.amount - keyIndex) % this.amount];
+          result.push(cryptValue);
+        }
+
+        keyI += 1;
+      }    
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+    return (this.direction) ? result.join('') : result.reverse().join('');
+  }
 }
 
 module.exports = VigenereCipheringMachine;
