84ae566e9165adda7cb027be86d5ffb9b7dd98aa
Eugenia Shibkova <shibkova91@gmail.com>
2020-02-29 03:29:24 +0300

    feat: add full solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,28 +2,38 @@ module.exports = function transform(arr) {
   if (!Array.isArray(arr)) {
     throw new Error;
   }
+  if (arr.length === 0) return arr;
 
   const result = [];
+  const first = arr[0];
+  const length = arr.length;
+  const controlSequences = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
 
-  arr.forEach((el, index) => {
-    switch (el) {
+  for (let i = 0; i < length; i += 1) {
+    switch (arr[i]) {
       case '--discard-next':
-        result.splice(index + 1, 1);
+        i += 1;
         break;
       case '--discard-prev':
         result.pop();
         break;
       case '--double-next':
-        result.push(arr[index + 1]);
+        if (i + 1 !== length) {
+          result.push(arr[i + 1]);
+        }
         break;
       case '--double-prev':
-        result.push(arr[index - 1]);
+        if ((i - 1) >= 0) {
+          result.push(arr[i - 1]);
+        }
         break;     
       default:
-        result.push(el);
+        if (!controlSequences.includes(arr[i])) {
+          result.push(arr[i]);
+        }
         break;
-    }       
-  });
-  
+    } 
+  }  
+
   return result;
 };
